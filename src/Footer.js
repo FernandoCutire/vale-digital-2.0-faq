62627e41dcd4d4cfb5372609d5d07c1cd64039c6
Fernando Cutire ubuntu <fernandocutire@gmail.com>
2021-07-06 11:34:04 -0500

    FIX: Dependencias - actualizacion de dependencias

--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,13 @@ function Footer () {
 	return (
 		<>
 		<footer>
-			<h2>Creado por <a target='_blank' href='https://twitter.com/fernandocutire'>@fernandocutire</a></h2>
-      <p>Open Source en: <a target='_blank' href='https://github.com/FernandoCutire/vale-digital-2.0-faq'>vale-digital-2.0-faq</a></p>
+			<h2>Creado por <a target='_blank' rel="noopener noreferrer" href='https://twitter.com/fernandocutire'>@fernandocutire</a></h2>
+      <p>Open Source en: <a target='_blank' rel="noopener noreferrer" href='https://github.com/FernandoCutire/vale-digital-2.0-faq'>vale-digital-2.0-faq</a></p>
 			<span className='footer-span'>Información relacionado al bono digital y en especial a la plataforma del INADEH 
 				para realizar los cursos. <b>El contenido es de mi autoría, y no debe ser una palabra formal o final sobre el tema.
 				Se emplean fuentes 
-				como <a target='_blank' href='https://twitter.com/search?q=vale%20digital%20&src=typed_query'>noticias nacionales</a> y 
-				preguntas de <a target='_blank' href='https://www.telemetro.com/'>la población en twitter.</a> 
+				como <a target='_blank' rel="noopener noreferrer" href='https://twitter.com/search?q=vale%20digital%20&src=typed_query'>noticias nacionales</a> y 
+				preguntas de <a target='_blank' rel="noopener noreferrer" href='https://www.telemetro.com/'>la población en twitter.</a> 
 				Para detalles exactos informarse en medios nacionales. </b>
 				Este es un sitio hosteado en un servidor gratuito de netlify, por lo que está sujeto a pérdidas por mucho tráfico.
 			</span>
